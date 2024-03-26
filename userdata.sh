ccf06beb3d522900c9893c1fe56456d2006b16aa
Precious Akpan <67706629+preciousakpan@users.noreply.github.com>
2024-03-26 22:25:15 +0100

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y nodejs npm mongodb
 
-git clone https://github.com/nodejs-app.git
+git clone https://github.com/Jacreator/NestJS_UserAuthAndReset
 cd nodejs-app
 npm install
 node server.js
